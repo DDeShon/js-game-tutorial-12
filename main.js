cbb3ad2e005ae5664605fc7da174f3d9b3ed6d5c
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-14 23:12:48 -0500

    working on player class

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { Player } from "./player.js";
+
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
@@ -8,8 +10,21 @@ window.addEventListener("load", function () {
     constructor(width, height) {
       this.width = width;
       this.height = height;
+      this.player = new Player(this);
     }
     update() {}
-    draw() {}
+    draw(context) {
+      this.player.draw(context);
+    }
   }
+
+  const game = new Game(canvas.width, canvas.height);
+  console.log(game);
+
+  function animate() {
+    // ctx.clearRect(0, 0, this.width, this.height);
+    game.draw(ctx);
+    requestAnimationFrame(animate);
+  }
+  animate();
 });
--- player.js
+++ player.js
@@ -1,5 +1,14 @@
-class Player {
+export class Player {
   constructor(game) {
     this.game = game;
+    this.width = 100;
+    this.height = 91.3;
+    this.x = this.game.width / 2 - this.width;
+    this.y = this.game.height - this.height;
+  }
+  update() {}
+  draw(context) {
+    context.fillStyle = "red";
+    context.fillRect(this.x, this.y, this.width, this.height);
   }
 }
