87f63900b310f9ac6be990bb52a0ad7b9b4249aa
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-17 18:38:12 -0500

    working on input class

--- input.js
+++ input.js
@@ -6,19 +6,21 @@ export class InputHandler {
         (e.key === "ArrowDown" ||
           e.key === "ArrowUp" ||
           e.key === "ArrowLeft" ||
-          e.key === "ArrowRight") &&
+          e.key === "ArrowRight" ||
+          e.key === " ") &&
         this.keys.indexOf(e.key) === -1
       ) {
-        this.keys.push(e.key);
+        this.keys.push(e.key, this.keys);
       }
-      console.log(e.key, this.keys);
+      console.log(e);
     });
     window.addEventListener("keyup", (e) => {
       if (
         e.key === "ArrowDown" ||
         e.key === "ArrowUp" ||
         e.key === "ArrowLeft" ||
-        e.key === "ArrowRight"
+        e.key === "ArrowRight" ||
+        e.key === " "
       ) {
         this.keys.splice(this.keys.indexOf(e.key), 1);
       }
