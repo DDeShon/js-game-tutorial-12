71c668336259f14c856fe39fdff2dcd2555cd9cc
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-18 05:07:39 -0500

    working on player movement

--- player.js
+++ player.js
@@ -5,12 +5,26 @@ export class Player {
     this.height = 91.3;
     this.x = this.game.width / 2 - this.width;
     this.y = this.game.height - this.height;
+    this.velocityY = 0;
+    this.weight = 1;
     this.image = document.getElementById("player");
+    this.speed = 0;
+    this.maxSpeed = 10;
   }
   update(input) {
-    if (input.includes("ArrowRight")) this.x++;
-    else if (input.includes("ArrowLeft")) this.x--;
-    else if (input.includes(" ")) this.y--;
+    // horizontal movement
+    this.x += this.speed;
+    if (input.includes("ArrowRight")) this.speed = this.maxSpeed;
+    else if (input.includes("ArrowLeft")) this.speed = -this.maxSpeed;
+    else this.speed = 0;
+    if (this.x < 0) this.x = 0;
+    if (this.x > this.game.width - this.width)
+      this.x = this.game.width - this.width;
+    // vertical movement
+    if (input.includes(" ") && this.onGround()) this.velocityY -= 30;
+    this.y += this.velocityY;
+    if (!this.onGround()) this.velocityY += this.weight;
+    else this.velocityY = 0;
   }
   draw(context) {
     context.drawImage(
@@ -24,5 +38,8 @@ export class Player {
       this.width,
       this.height
     );
+  }
+  onGround() {
+    return this.y >= this.game.height - this.height;
   }
 }
