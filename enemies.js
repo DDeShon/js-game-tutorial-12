f7e52bc001ac22e08effc788c945162da7ed352f
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-21 15:33:06 -0500

    working on enemy class

--- enemies.js
+++ enemies.js
@@ -6,11 +6,34 @@ class Enemy {
     this.frameInterval = 1000 / this.fps;
     this.frameTimer = 0;
   }
-  update() {}
+  update(deltaTime) {
+    // movement
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this.frameTimer > this.frameInterval) {
+      this.frameTimer = 0;
+      if (this.frameX < this.maxFrame) this.frameX++;
+      else this.frameX = 0;
+    } else {
+      this.frameTimer += deltaTime;
+    }
+  }
   draw() {}
 }
 
-class FlyingEnemy extends Enemy {}
+class FlyingEnemy extends Enemy {
+  constructor(game) {
+    super();
+    this.game = game;
+    this.width = 60;
+    this.height = 44;
+    this.x = 200;
+    this.y = 200;
+    this.speedX = 2;
+    this.maxFrame = 5;
+    this.image = document.getElementById("enemy_fly");
+  }
+}
 
 class GroundEnemy extends Enemy {}
 
