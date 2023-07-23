26055ec7d46e3eedc04c457805a5c54aadf7d728
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-23 09:36:50 -0500

    working on ui

--- UI.js
+++ UI.js
@@ -5,8 +5,11 @@ export class UI {
     this.fontFamily = "Helvetica";
   }
   draw(context) {
-    context.font = this.fontSize + "px" + this.fontFamily;
+    context.font = this.fontSize + "px " + this.fontFamily;
     context.textAlign = "left";
     context.fillStyle = this.game.fontColor;
+
+    // score
+    context.fillText("Score: " + this.game.score, 20, 50);
   }
 }
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
 import { Background } from "./background.js";
 import { FlyingEnemy, GroundEnemy, ClimbingEnemy } from "./enemies.js";
+import { UI } from "./UI.js";
 
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
@@ -19,6 +20,7 @@ window.addEventListener("load", function () {
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler(this);
+      this.UI = new UI(this);
       this.enemies = [];
       this.enemyTimer = 0;
       this.enemyInterval = 1000;
@@ -48,6 +50,7 @@ window.addEventListener("load", function () {
       this.enemies.forEach((enemy) => {
         enemy.draw(context);
       });
+      this.UI.draw(context);
     }
     addEnemy() {
       if (this.speed > 0 && Math.random() < 0.8)
