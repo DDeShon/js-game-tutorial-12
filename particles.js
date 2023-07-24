2dc6cfb8241d3f599898b08c7890ab11bd4d04ea
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-24 17:13:43 -0500

    working on dust particles

--- particles.js
+++ particles.js
@@ -3,4 +3,26 @@ class Particle {
     this.game = game;
     this.markedForDeletion = false;
   }
+  update() {
+    this.x -= this.speedX + this.game.speed;
+    this.y -= this.speedY;
+    this.size *= 0.95;
+    if (this.size < 0.5) this.markedForDeletion = true;
+  }
 }
+
+export class Dust extends Particle {
+  constructor(game, x, y) {
+    super(game);
+    this.size = Math.random() * 10 + 10;
+    this.x = x;
+    this.y = y;
+    this.speedX = Math.random();
+    this.speedY = Math.random();
+    this.color = "black";
+  }
+}
+
+export class Dust extends Particle {}
+
+export class Dust extends Particle {}
