33a7293ab24fa1d2e366d681e2d36464f37e5d9d
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-07-25 20:42:44 -0500

    working on collision animation

---
+++ collisionAnimation.js
@@ -0,0 +1,11 @@
+class CollisionAnimation {
+  constructor(game, x, y) {
+    this.game = game;
+    this.image = document.getElementById("collisionAnimation");
+    this.spriteWidth = 100;
+    this.spriteHeight = 90;
+    this.sizeModifier = Math.random() + 0.5;
+    this.width = this.spriteWidth * this.sizeModifier;
+    this.height = this.spriteHeight * this.sizeModifier;
+  }
+}
